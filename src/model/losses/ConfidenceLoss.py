3db68d1915bf560584cb686306f13bb64c07db6f
Qwagperson <gsantelicesn2@gmail.com>
2024-05-20 14:00:18 -0400

    Changed confidenceloss

--- src/model/losses/ConfidenceLoss.py
+++ src/model/losses/ConfidenceLoss.py
@@ -4,7 +4,6 @@ from torch import nn
 class ConfidenceLoss(nn.Module):
     def __init__(self):
         super().__init__()
-        self.binary_cross_entropy = nn.BCELoss()
 
     def forward(self, conf_pred, conf_true):
-        return self.binary_cross_entropy(input=conf_pred, target=conf_true)
+        return nn.functional.binary_cross_entropy(conf_pred, conf_true)
