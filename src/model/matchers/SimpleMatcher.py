62832a13331ccec9e2d564c24dd7cebafc502659
Andrea Ranieri <andrea.ranieri@cnr.it>
2024-05-24 17:28:37 +0200

    Replaced get_optimal_assignment_mse() with get_fixed_assignment()

--- src/model/matchers/SimpleMatcher.py
+++ src/model/matchers/SimpleMatcher.py
@@ -22,9 +22,43 @@ class SimpleMatcher:
 
 
 
-    def get_optimal_assignment_mse(self, points, y_pred, y_true):
+    def get_fixed_assignment(self, points, y_pred, y_true):
+        """
+        :param points: N x 3
+        :param y_pred: M x 7
+        :param y_true: K x 6
+        :return:
         """
 
+        print(f'----------------------')
+        print(f'----------------------')
+        print(f'get_fixed_assignment()')
+        print(f'----------------------')
+        print(f'----------------------')
+
+        print(f'y_pred orig: {y_pred}\ny_true orig: {y_true}')
+
+        m = y_pred.shape[0]
+        ngt = y_true.shape[0]
+        #cost_matrix = self.method(points, y_pred.detach().clone(), y_true)
+        #row_id, col_id = linear_sum_assignment(cost_matrix.cpu().detach().numpy())
+        row_id, col_id = list(range(ngt)), list(range(ngt))
+
+        print(f'm: {m}, row_id: {row_id}, col_id: {col_id}')
+        #print(f'cost_matrix: {cost_matrix}')
+
+        c_hat = create_onehot(row_id, m, device=self.device)
+
+        print(f'c_hat: {c_hat}')
+
+        y_pred = y_pred[row_id, :]
+        y_true = y_true[col_id, :]
+
+        print(f'y_pred: {y_pred}\ny_true: {y_true}')
+
+        return c_hat, y_pred, y_true, row_id, col_id
+    def get_optimal_assignment_mse(self, points, y_pred, y_true):
+        """
         :param points: N x 3
         :param y_pred: M x 7
         :param y_true: K x 6
@@ -59,7 +93,6 @@ class SimpleMatcher:
 
     def get_optimal_assignment_aux(self, points, y_pred, y_true):
         """
-
         :param points: N x 3
         :param y_pred: M x 7
         :param y_true: K x 6
@@ -110,7 +143,8 @@ class SimpleMatcher:
                 assignments_y_true_2_y_pred.append(None)
             else:
                 #c_hat, match_y_pred, match_y_true, pred2true, true2pred = self.get_optimal_assignment_aux(points[idx], y_pred[idx], y_true[idx])
-                c_hat, match_y_pred, match_y_true, pred2true, true2pred = self.get_optimal_assignment_mse(points[idx], y_pred[idx], y_true[idx])
+                #c_hat, match_y_pred, match_y_true, pred2true, true2pred = self.get_optimal_assignment_mse(points[idx], y_pred[idx], y_true[idx])
+                c_hat, match_y_pred, match_y_true, pred2true, true2pred = self.get_fixed_assignment(points[idx], y_pred[idx], y_true[idx])
                 c_hats.append(c_hat)
                 matches_y_pred.append(match_y_pred)
                 matches_y_true.append(match_y_true)
