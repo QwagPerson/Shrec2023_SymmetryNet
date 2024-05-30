ed0836c92786553bb2bf812973db2901dc5a7ffe
Qwagperson <gsantelicesn2@gmail.com>
2024-05-29 23:48:49 -0400

    run script updated

--- mass_run.sh
+++ mass_run.sh
@@ -1,10 +1,2 @@
-PYTHONPATH=. python main.py fit --config config/centerNnormals/experimentos_por_clase/astroid.yaml 
-PYTHONPATH=. python main.py fit --config config/centerNnormals/experimentos_por_clase/citrus.yaml 
-PYTHONPATH=. python main.py fit --config config/centerNnormals/experimentos_por_clase/cylinder.yaml 
-PYTHONPATH=. python main.py fit --config config/centerNnormals/experimentos_por_clase/egg_keplero.yaml 
-PYTHONPATH=. python main.py fit --config config/centerNnormals/experimentos_por_clase/geometric_petal.yaml 
-PYTHONPATH=. python main.py fit --config config/centerNnormals/experimentos_por_clase/lemniscate.yaml 
-PYTHONPATH=. python main.py fit --config config/centerNnormals/experimentos_por_clase/m_convexities.yaml 
-PYTHONPATH=. python main.py fit --config config/centerNnormals/experimentos_por_clase/mouth_curve.yaml 
-PYTHONPATH=. python main.py fit --config config/centerNnormals/experimentos_por_clase/revolution.yaml 
-PYTHONPATH=. python main.py fit --config config/centerNnormals/experimentos_por_clase/square.yaml 
+PYTHONPATH=. python main.py fit --config ./config/centerNnormals/proposed_experiments/sym-10k-xz-v1.0-repack-rotprob0.5-onlyrotx-experiment-config.yaml
+PYTHONPATH=. python main.py fit --config ./config/centerNnormals/proposed_experiments/sym-100k-xz-v1.0-repack-rotprob0.5-onlyrotx-experiment-config.yaml
